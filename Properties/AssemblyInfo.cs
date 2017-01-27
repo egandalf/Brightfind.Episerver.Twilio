89de943d04452833a7253c3ccd3a29189c3ea1e5
egandalf <jamesstout@Jamess-MacBook-Pro.local>
2017-01-27 10:10:20 -0500

    Updated nuget process to better specify version and dependencies.
    Added beta flag. Corrected dependencies (example: Epi 10.0 instead of
    10.3.2). Bumped version to 0.2 - and will maintain 0.x until considered
    out of prerelease.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Brightfind.Episerver.Twilio")]
-[assembly: AssemblyDescription("Plug-and-Play integration with Twilio")]
+[assembly: AssemblyDescription("Plug-and-Play integration with Twilio for Episerver CMS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Brightfind")]
 [assembly: AssemblyProduct("Brightfind.Episerver.Twilio")]
